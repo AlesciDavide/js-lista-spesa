9ad78383ec0a297d7c41479b0b421b5d17c8d88a
AlesciDavide <davi.alesci@email.com>
2024-05-15 14:28:37 +0200

    aggiunto style e modificato l'ul con ol

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const listaSpesa = ['ciliegie', 'patatine', 'birra', 'acqua', 'cocacola', 'ciocc
 let i = 0;
 
 
-const ulElements = document.querySelector('ul#lista_dom');
+const ulElements = document.querySelector('ol#lista_dom');
 
 while(i < listaSpesa.length){
     let liElements = document.createElement('li');
