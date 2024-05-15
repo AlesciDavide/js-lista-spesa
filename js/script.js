47743574c1ea1cb51bd96abff3f70663a4a9a2b1
AlesciDavide <davi.alesci@email.com>
2024-05-15 14:27:02 +0200

    creato l'elemento li, aggiunta la classe all'elemento e popolato il li con il contenuto dell'array e stampato in pagina

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ let i = 0;
 const ulElements = document.querySelector('ul#lista_dom');
 
 while(i < listaSpesa.length){
-    
-    
+    let liElements = document.createElement('li');
+    liElements.classList.add('My_li');
+    liElements.append(listaSpesa[i]);
+    ulElements.appendChild(liElements);
     i++
 }
